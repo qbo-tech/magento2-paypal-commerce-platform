d24db5348c194dbb374d411e7abc7b09da15cf69
Nestor Sanchez <nestor.sanchez@qbo.tech>
2025-05-09 08:01:50 -0500

    removed plugin and change error message

--- Controller/Financing/Index.php
+++ Controller/Financing/Index.php
@@ -54,7 +54,7 @@ class Index extends \Magento\Framework\App\Action\Action
 
         } catch (\Exception $e) {
             $this->_loggerHandler->error($e->getMessage());
-            $resultJson->setData(array('reason' => $e->getMessage()));
+            $resultJson->setData(array('reason' => __('An error has occurred on the server, please try again later')));
             return $resultJson->setHttpResponseCode(500);
         }
 
